2d3c69642e65107b4fbcb78a1824b4acb4da27ac
Aditya Saxena <100509451+aaddii091@users.noreply.github.com>
2023-02-04 23:57:39 +0530

    Add files via upload

--- hackathon/Main Page/main.js
+++ hackathon/Main Page/main.js
@@ -1,2 +1,17 @@
-console.log("hi");
-console.log(localStorage.getItem("Full_Name"));
+const hide = document.querySelector(".modal");
+const blur = document.querySelector(".whole");
+const body = document.querySelector(".bodyflow");
+
+function user() {
+  console.log("");
+  hide.classList.add("hidden");
+  blur.classList.remove("whole");
+  body.classList.remove("bodyflow");
+  // event.preventDefault();
+}
+function client() {
+  // event.preventDefault();
+  // hide.classList.add("hidden");
+  // blur.classList.remove("whole");
+  // body.classList.remove("bodyflow");
+}
